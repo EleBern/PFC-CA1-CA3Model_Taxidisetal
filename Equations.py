a4f9a590c3e270aed34f4ca53a6264d71cb3ed1a
Eleonora <82338767+EleBern@users.noreply.github.com>
2022-09-01 17:52:48 +0100

    Updated equations and parameters

--- Equations.py
+++ Equations.py
@@ -1,91 +1,10 @@
 from brian2 import *
 from Parameters import *
 
-# PFC PYRAMIDAL CELL Equations--------------------------------------------------
-eqs_PYc = '''
-    dVs/dt = (-( I_L + I_Na + I_K + I_A + I_KS + I_KNa ) - I_sd/As -I_GABA_IP + Is) / C_c /ms  : 1 
-    dVd/dt = (-( I_NaP + I_AR + I_Ca + I_KCa ) + I_sd/Ad -I_AMPA_PP - I_C_HP_PP + Id) / C_c /ms: 1
-    
-    # dVs/dt = (-( I_L + I_Na + I_K + I_A + I_KS + I_KNa ) - I_sd/As + Is) / C_c /ms           : 1 
-    # dVd/dt = (-( I_NaP + I_AR + I_Ca + I_KCa ) + I_sd/Ad + Id) / C_c /ms       : 1
-    
-    dNa/dt = ( -alpha_Na*(As*I_Na + Ad*I_NaP)  -  R_pump*( Na**3/(Na**3+15.**3)\
-    - Na_eq**3/(Na_eq**3+15.**3) ) )/ms                                          : 1
-
-    dCa/dt = (-alpha_Ca*Ad*I_Ca - Ca/tau_Ca) /ms                                 : 1
-    
-#SOMATIC currents    
-# Sodium Current  
-    m = alpham / (alpham + betam)                                                : 1
-    dh/dt = phi* (alphah*(1.-h)-betah*h) /ms                                     : 1
-    # alpham = 0.1*(Vs+33.) / (1.-exp(-(Vs+33.)/10.))                            : 1
-    alpham = 0.1*10 / exprel(-(Vs+33.)/10.)                                      : 1
-    betam = 4.*exp(-(Vs+53.7)/12.)                                               : 1
-    alphah = 0.07*exp(-(Vs+50.)/10.)                                             : 1
-    betah = 1./(1.+exp(-(Vs+20.)/10.))                                           : 1
-    I_Na = gNa_c*(m**3)*h*(Vs-VNa_c)                                             : 1
-
-# Potassium Current   
-    dn/dt = phi* (alphan*(1.-n)-betan*n) /ms                                     : 1
-    # alphan = 0.01*(Vs+34.) / (1.-exp(-(Vs+34.)/10.))                           : 1
-    alphan = 0.01*10 / exprel(-(Vs+34.)/10.)                                     : 1
-    betan = 0.125*exp(-(Vs+44.)/25.)                                             : 1
-    I_K = gK_c*(n**4)*(Vs-VK_c)                                                  : 1
-    
-# Leakage Current    
-    I_L = gL*(Vs-VL)                                                             : 1
-
-# Fast A-type K+ Current    
-    mA_inf = 1./(1.+exp(-(Vs+50.)/20.))                                          : 1
-    dhA/dt = (hA_inf-hA)/tau_hA    /ms                                           : 1
-    hA_inf = 1./(1.+exp((Vs+80.)/6.))                                            : 1
-    I_A = gA_c*(mA_inf**3)*hA*(Vs-VK_c)                                          : 1
-
-# Non-inactivating slow K+ Current (Wang 1999a)
-    dmKS/dt = (mKS_inf-mKS)/tau_mKS   /ms                                        : 1
-    mKS_inf = 1./(1.+exp(-(Vs+34.)/6.5))                                         : 1
-    tau_mKS = 8./(exp(-(Vs+55.)/30.) + exp((Vs+55.)/30.))                        : 1 
-    I_KS = gKS_c*mKS*(Vs-VK_c)                                                   : 1
-
-# Na+-dependent K+ current  
-    w = 0.37/(1.+(38.7/Na)**3.5)                                                 : 1
-    I_KNa = gKNa_c*w*(Vs-VK_c)                                                   : 1
-    
-#DENDRITIC Currents
-# Presistent Sodium Current
-    mNaP_inf = 1./(1.+exp(-(Vd+55.7)/7.7))                                       : 1
-    I_NaP = gNaP_c*(mNaP_inf**3)*(Vd-VNa_c)                                      : 1
-
-# Inward rectifier K+ Current
-    hAR_inf = 1./(1.+exp((Vd+75.)/4.))                                           : 1
-    I_AR = gAR_c*hAR_inf*(Vd-VK_c)                                               : 1
-
-# High-Threshold Ca+ Current
-    mCa_inf = 1./(1.+exp(-(Vd+20.)/9.))                                          : 1
-    I_Ca = gCa_c*(mCa_inf**2)*(Vd-VCa_c)                                         : 1
-
-# Ca+-depndent K+ Current    
-    I_KCa = gKCa_c*(Ca/(Ca+K_D))*(Vd-VK_c)                                       : 1
- 
-# Dendro-Somatic Current    
-    I_sd = gsd*(Vs-Vd)                                                           : 1
-
-#VARYING Network Paramteres
-    VL                                                                           : 1
-    gL                                                                           : 1
-    gsd                                                                          : 1
-    Id                                                                           : 1
-    Is                                                                           : 1
-    I_AMPA_PP                                                                    : 1
-    I_GABA_IP                                                                    : 1
-    I_C_HP_PP                                                                    : 1
-'''
-#-------------------------------------------------------------------------------
-
 # HPC PYRAMIDAL CELL Equations--------------------------------------------------
 eqs_PYh = '''
     dVs/dt = (-( I_Ls + I_Na + I_K ) - I_sd/p - I_GABA_IP/p + Is/p)/C_h /ms      : 1
-    dVd/dt = (-( I_Ld + I_Ca + I_KAHP + I_KCa ) + I_sd/(1.-p) - I_AMPA_PP/(1.-p) - I_C_HP_PP/(1.-p) + Id/(1.-p))/C_h /ms : 1
+    dVd/dt = (-( I_Ld + I_Ca + I_KAHP + I_KCa ) + I_sd/(1.-p) - I_AMPA_PP/(1.-p) + Id/(1.-p))/C_h /ms : 1
     dCa/dt = (-0.13*I_Ca - 0.075*Ca) /ms                                         : 1
     
 #SOMATIC currents    
@@ -144,13 +63,12 @@ eqs_PYh = '''
     Id                                                                           : 1
     I_AMPA_PP                                                                    : 1
     I_GABA_IP                                                                    : 1
-    I_C_HP_PP                                                                    : 1
 '''
 #-------------------------------------------------------------------------------
 
 # INTERNEURON Equations---------------------------------------------------------
 eqs_IN = '''
-    dVi/dt = ((-( I_Li + I_Nai + I_Ki ) - I_AMPA_PI - I_GABA_II - I_C_HP_PI + Ii) / Ci) /ms  : 1 
+    dVi/dt = ((-( I_Li + I_Nai + I_Ki ) - I_AMPA_PI - I_GABA_II + Ii) / Ci) /ms  : 1 
     
 # Sodium Current
     mi = alphami / (alphami + betami)                                            : 1
@@ -178,7 +96,6 @@ eqs_IN = '''
     Ii                                                                           : 1
     I_AMPA_PI                                                                    : 1
     I_GABA_II                                                                    : 1
-    I_C_HP_PI                                                                    : 1
 '''
 #-------------------------------------------------------------------------------
 
@@ -189,7 +106,6 @@ SynAMPA_PP = '''
     Isingle_AMPA_PP = gAMPA_PP * r_AMPAPP * (Vd - Erev_AMPA)                                     : 1 
     I_AMPA_PP_post = Isingle_AMPA_PP                                                             : 1 (summed) 
     gAMPA_PP                                                                                     : 1
-    Alpha_AMPA                                                                                   : 1
     lastupdate_AMPAPP                                                                            : second 
     '''
     
@@ -205,7 +121,7 @@ SynGABA_IP = '''
 SynGABA_II ='''   
     dr_GABAII/dt = (Alpha_GABA * C_GABAII * (1 - r_GABAII) - Beta_GABA * r_GABAII) /ms           : 1 (clock-driven)
     C_GABAII = int(timestep(t - lastupdate_GABAII, dt) <= timestep(Cdur_GABA*ms, dt))* Cmax_GABA : 1
-    Isingle_GABA_II = gGABA_II * r_GABAII * (Vi - Erev_GABA)                                     : 1  
+    Isingle_GABA_II = gGABA_II * r_GABAII * (Vi - Erev_GABA)                                       : 1  
     I_GABA_II_post = Isingle_GABA_II                                                             : 1 (summed)
     gGABA_II                                                                                     : 1
     lastupdate_GABAII                                                                            : second  
@@ -214,35 +130,15 @@ SynGABA_II ='''
 SynAMPA_PI = '''
     dr_AMPAPI/dt = (Alpha_AMPA * C_AMPAPI * (1 - r_AMPAPI) - Beta_AMPA * r_AMPAPI) /ms           : 1 (clock-driven)
     C_AMPAPI = int(timestep(t - lastupdate_AMPAPI, dt) <= timestep(Cdur_AMPA*ms, dt))* Cmax_AMPA : 1
-    Isingle_AMPA_PI = gAMPA_PI * r_AMPAPI * (Vi - Erev_AMPA)                                     : 1 
+    Isingle_AMPA_PI = gAMPA_PI * r_AMPAPI * (Vi - Erev_AMPA)                                       : 1 
     I_AMPA_PI_post = Isingle_AMPA_PI                                                             : 1 (summed)
     gAMPA_PI                                                                                     : 1
-    Alpha_AMPA                                                                                   : 1
     lastupdate_AMPAPI                                                                            : second  
-    '''
-
-SynC_HP_PI = '''
-    dr_C_HPPI/dt = (Alpha_AMPA * C_C_HPPI * (1 - r_C_HPPI) - Beta_AMPA * r_C_HPPI) /ms           : 1 (clock-driven)
-    C_C_HPPI = int(timestep(t - lastupdate_C_HPPI, dt) <= timestep(Cdur_AMPA*ms, dt))* Cmax_AMPA : 1
-    Isingle_C_HP_PI = gAMPA_PI * r_C_HPPI * (Vi - Erev_AMPA)                                     : 1 
-    I_C_HP_PI_post = Isingle_C_HP_PI                                                             : 1 (summed)
-    gAMPA_PI                                                                                     : 1
-    Alpha_AMPA                                                                                   : 1
-    lastupdate_C_HPPI                                                                            : second  
-    '''
-    
-SynC_HP_PP = '''
-    dr_C_HPPP/dt = (Alpha_AMPA * C_C_HPPP * (1 - r_C_HPPP) - Beta_AMPA * r_C_HPPP) /ms           : 1 (clock-driven)
-    C_C_HPPP = int(timestep(t - lastupdate_C_HPPP, dt) <= timestep(Cdur_AMPA*ms, dt))* Cmax_AMPA : 1
-    Isingle_C_HP_PP = gAMPA_PP * r_C_HPPP * (Vd - Erev_AMPA)                                     : 1 
-    I_C_HP_PP_post = Isingle_C_HP_PP                                                             : 1 (summed)
-    gAMPA_PP                                                                                     : 1
-    Alpha_AMPA                                                                                   : 1
-    lastupdate_C_HPPP                                                                            : second  
     '''
 #-------------------------------------------------------------------------------
     
 # Equations for when there is a presinaptic spike-------------------------------
+
 GABA_IP_fire = '''
     lastupdate_GABAIP = int(timestep(t - lastupdate_GABAIP, dt) > timestep(Deadtime_GABA*ms, dt)) * t \
     + int(timestep(t - lastupdate_GABAIP, dt) < timestep(Deadtime_GABA*ms, dt)) * lastupdate_GABAIP  
@@ -260,15 +156,5 @@ AMPA_PP_fire = '''
 AMPA_PI_fire = '''
     lastupdate_AMPAPI = int(timestep(t - lastupdate_AMPAPI, dt) > timestep(Deadtime_AMPA*ms, dt)) * t \
     + int(timestep(t - lastupdate_AMPAPI, dt) < timestep(Deadtime_AMPA*ms, dt)) * lastupdate_AMPAPI  
-'''
-
-C_HP_PI_fire = '''
-    lastupdate_C_HPPI = int(timestep(t - lastupdate_C_HPPI, dt) > timestep(Deadtime_AMPA*ms, dt)) * t \
-    + int(timestep(t - lastupdate_C_HPPI, dt) < timestep(Deadtime_AMPA*ms, dt)) * lastupdate_C_HPPI  
-'''
-
-C_HP_PP_fire = '''
-    lastupdate_C_HPPP = int(timestep(t - lastupdate_C_HPPP, dt) > timestep(Deadtime_AMPA*ms, dt)) * t \
-    + int(timestep(t - lastupdate_C_HPPP, dt) < timestep(Deadtime_AMPA*ms, dt)) * lastupdate_C_HPPP  
 '''
 #-------------------------------------------------------------------------------
--- Parameters.py
+++ Parameters.py
@@ -2,10 +2,7 @@ from brian2 import *
 
 # mV, cm, ms, msiemens/cm**2, uA/cm**2, ufarad/cm**2, mmole   
 
-# NETWORK & CELL SIZES (in cm**2)-----------------------------------------------
-N_PYc = 1000         
-N_INc = 250
-          
+# NETWORK & CELL SIZES (in cm**2)-----------------------------------------------          
 N_PY3 = 1000
 N_IN3 = 100
 
@@ -14,45 +11,13 @@ N_IN1 = 100
 
 N_PYh = N_PY3 + N_PY1
 N_INh = N_IN3 + N_IN1
-N_IN  = N_INc + N_INh
+N_IN  = N_INh
 
-Gc = N_PYc + N_INc     # total number of cells
-L = 0.5                # total length of PFC network (5*mm)
 
 As = 1.5e-4     # Dendrtic compartment size (0.015*(mm**2))
 Ad = 3.5e-4     # Axosomatic compartment size (0.035*(mm**2)) 
 S_PY = As + Ad  # Total pyramidal cell size (0.05*(mm**2))
 S_IN = 2e-4     # Total interneuron size (0.02*(mm**2))
-#-------------------------------------------------------------------------------
-
-# CORTICAL PYRAMIDAL CELLS Parameters-------------------------------------------
-C_c = 1.   
-phi = 4.      
-gNa_c = 50. 
-gK_c = 10.5  
-gA_c = 1.   
-gKS_c = 0.576  
-gKNa_c = 1.33      
-gCa_c = 0.43    
-gKCa_c = 0.57   
-gNaP_c = 0.0686 
-gAR_c = 0.0257  
-
-alpha_Na = 10.    # 0.01*mmole*(nA**-1)*(ms**-1)  turned to mmole*(uA**-1)*(ms**-1)
-R_pump = 0.018    # mmole*ms**-1
-Na_eq = 9.5       # mmole
-alpha_Ca = 0.005  # umole*(nA**-1)*(ms**-1)   same as  mmole*(uA**-1)*(ms**-1)
-K_D = 0.03        # mmole
-tau_hA = 15.
-tau_Ca = 150. 
-
-VL_c = -60.95 
-gL_c = 0.0667
-gsd_c = 1.75e-3   # msiemens
-
-VK_c  = -100.
-VNa_c = 55.
-VCa_c = 120.
 #-------------------------------------------------------------------------------
 
 # HIPPOCAMPAL PYRAMIDAL CELLS Parameters----------------------------------------
@@ -87,10 +52,29 @@ gLi_m = 0.1025
 # ------------------------------------------------------------------------------
 
 # SYNAPTIC Parameters-----------------------------------------------------------
-gAMPA_PP_c = 30* 5.4*(1.e-6/Ad)    #29.3, 29.5, 30
-gAMPA_PI_c = 10*2.25*(1.e-6/S_IN)
-gGABA_IP_c = 4.15*(1.e-6/As)  #1-6
-gGABA_II_c = 10*0.165*(1.e-6/S_IN) 
+# gAMPA_PP_3 = 1.*(1.e-6/S_PY)  
+# gAMPA_PI_3 = 1.*(1.e-6/S_IN)  
+# gGABA_IP_3 = 1.*(1.e-6/S_PY) 
+# gAMPA_PP_1 = 1.*(1.e-6/S_PY) 
+# gAMPA_PI_1 = 1.*(1.e-6/S_IN)  
+# gGABA_IP_1 = 1.*(1.e-6/S_PY)
+# gGABA_II_1 = 1.*(1.e-6/S_IN)
+
+# gAMPA_PP_3 = 70.*(1.e-6/S_PY)  
+# gAMPA_PI_3 = 70.*(1.e-6/S_IN)  
+# gGABA_IP_3 = 70.*(1.e-6/S_PY) 
+# gAMPA_PP_1 = 70.*(1.e-6/S_PY) 
+# gAMPA_PI_1 = 70.*(1.e-6/S_IN)  
+# gGABA_IP_1 = 70.*(1.e-6/S_PY)
+# gGABA_II_1 = 70.*(1.e-6/S_IN)
+
+# gAMPA_PP_3 = 50.*(1.e-6/S_PY)  
+# gAMPA_PI_3 = 80.*(1.e-6/S_IN)  
+# gGABA_IP_3 = 100.*(1.e-6/S_PY) 
+# gAMPA_PP_1 = 80.*(1.e-6/S_PY) 
+# gAMPA_PI_1 = 80.*(1.e-6/S_IN)  
+# gGABA_IP_1 = 80.*(1.e-6/S_PY)
+# gGABA_II_1 = 40.*(1.e-6/S_IN)
 
 gAMPA_PP_h = 50.*(1.e-6/S_PY)  
 gAMPA_PI_h = 80.*(1.e-6/S_IN)  
@@ -100,6 +84,7 @@ gGABA_II_h = 40.*(1.e-6/S_IN)
 # Destexhe synaptic parameters
 # Taken from modelDB
 # Thalamocortical and Thalamic Reticular Network (Destexhe et al 1996)
+# GABAa
 Cmax_GABA = 0.5 
 Cdur_GABA = 0.3
 Alpha_GABA = 10.5
@@ -111,8 +96,7 @@ Deadtime_GABA = 1.
 Cmax_AMPA = 0.5 
 Cdur_AMPA = 0.3
 # Alpha_AMPA = 0.94
-Alpha_AMPA_h = 3.94
-Alpha_AMPA_c = 1.54
+Alpha_AMPA = 3.94
 Beta_AMPA = 0.18
 Erev_AMPA = 0.
 Deadtime_AMPA = 1.
@@ -124,13 +108,7 @@ V_ax_P = 0.5       # PY axonal conductance velocity (mm/msec)
 V_ax_I = 0.1       # IN axonal conductance velocity
 #-------------------------------------------------------------------------------
 
-# PFC CONNECTIVITY Parameters---------------------------------------------------
-kPPc = kPIc = kIPc = kIIc = 20. 
-sigma_PYc = 0.0250/(L/Gc)  # PY connectivity deviation in cell numbers
-sigma_INc = 0.0125/(L/Gc)  # IN connectivity deviation in cell numbers
-#-------------------------------------------------------------------------------
-
-# HPC CONNECTIVITY Parameters---------------------------------------------------
+# CONNECTIVITY Parameters-------------------------------------------------------
 kPPh = 200. 
 kPIh = 20.   
 kIPh = 400. 
@@ -139,20 +117,30 @@ sigma_PYh = 100.   # PY connectivity deviation in cell numbers
 sigma_INh = 30.    # IN connectivity deviation in cell numbers
 #-------------------------------------------------------------------------------
 
-# PFC-to-HPC CONNECTIVITY Parameters--------------------------------------------
-# alpha_PFC_CAh_P = 2.#50.
-# alpha_PFC_CAh_I = 2.
-kchP = 50.                  
-kchI = 2.
-sigma_chP = 50.
-sigma_chI = 50.
+# CA3 CONNECTIVITY Parameters---------------------------------------------------
+# kPP3 = 55. 
+# kPI3 = 5.   
+# kIP3 = 68. 
+# sigma_PY3 = 100.   # PY connectivity deviation in cell numbers
+# sigma_IN3 = 30.    # IN connectivity deviation in cell numbers
+# kPP3 = 200. 
+# kPI3 = 20.   
+# kIP3 = 400. 
+# sigma_PY3 = 100.   # PY connectivity deviation in cell numbers
+# sigma_IN3 = 30.    # IN connectivity deviation in cell numbers
 #-------------------------------------------------------------------------------
 
-# HPC-to-PFC CONNECTIVITY Parameters--------------------------------------------
-# alpha_CA1_PFC_P = 8.   
-# alpha_CA1_PFC_I = 13.   
-khcP = 50.
-khcI = 2.        
-sigma_hcP = 50.
-sigma_hcI = 50.
+# # CA1 CONNECTIVITY Parameters---------------------------------------------------
+# kPI1 = 20.  
+# kIP1 = 400.
+# kII1 = 100.
+# sigma_PY1 = 100.   # PY connectivity deviation in cell numbers
+# sigma_IN1 = 10.    # IN connectivity deviation in cell numbers
+# #-------------------------------------------------------------------------------
+
+# # SCHAFFER CONNECTIVITY Parameters----------------------------------------------
+# k31 = 130.         # number of connections per CA3 PY cell
+# sigma_31 = 120.    # connectivity deviation in cell numbers
+# multiP = 20.       # number of multiple synapses on each connection
+# multiI = 20.
 #-------------------------------------------------------------------------------
